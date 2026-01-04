a98f101f5c01b8d4835ed2660f3b46bc40c48c13
haashik <haashikph1992@gmail.com>
2026-01-04 19:50:30 +0530

    start game button added

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const box = document.getElementById("box");
 const gameArea = document.getElementById("game-area");
 const scoreDisplay = document.getElementById("score");
 const timeDisplay = document.getElementById("time");
-
+const startBtn = document.getElementById("start-btn");
 
 let score = 0;
 let timeLeft = 30;
+let timer = null;
+let gameRunning = false;
 
 // Move box randomly
 function moveBox() {
@@ -21,23 +23,54 @@ function moveBox() {
 
 // Click event
 box.addEventListener("click", () => {
+  if (!gameRunning) return;
   score++;
   scoreDisplay.textContent = score;
   moveBox();
 });
 
 // Timer
-const timer = setInterval(() => {
-  timeLeft--;
-  timeDisplay.textContent = timeLeft;
-
-  if (timeLeft === 0) {
-    clearInterval(timer);
-    box.style.display = "none";
-    alert("Game Over! Your score: " + score);
-  }
-}, 1000);
+//const timer = setInterval(() => {
+//  timeLeft--;
+//  timeDisplay.textContent = timeLeft;
+//
+//  if (timeLeft === 0) {
+//    clearInterval(timer);
+//    box.style.display = "none";
+//    alert("Game Over! Your score: " + score);
+//  }
+//}, 1000);
 
 
 // Start game
-moveBox();
+// moveBox();
+
+function startGame() {
+    if (gameRunning) return;
+    gameRunning = true;
+    score = 0;
+    timeLeft = 30;
+    scoreDisplay.textContent = score;
+    timeDisplay.textContent = timeLeft;
+    box.style.display = "block";
+    startBtn.style.display = "none";
+    moveBox();
+    timer = setInterval(() => {
+      timeLeft --;
+      timeDisplay.textContent = timeLeft;
+
+      if (timeLeft === 0){
+        endGame();
+      }
+    }, 1000);
+}
+
+function endGame(){
+  clearInterval(timer);
+  gameRunning = false;
+  box.style.display = "none";
+  startBtn.style.display = "inline-block";
+  alert("Game over! Your score: " + score);
+}
+
+startBtn.addEventListener("click",startGame);
